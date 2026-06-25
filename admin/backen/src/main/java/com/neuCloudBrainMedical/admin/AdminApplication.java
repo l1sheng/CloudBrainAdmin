f0ef5bfd75fec8f99283952f293123438f6fa4f8
张旭升 <2744932552@qq.com>
2026-06-25 21:31:45 +0800

    github,gitee同步

--- admin/backen/src/main/java/com/neuCloudBrainMedical/admin/AdminApplication.java
+++ admin/backen/src/main/java/com/neuCloudBrainMedical/admin/AdminApplication.java
@@ -2,7 +2,7 @@ package com.neuCloudBrainMedical.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//空白同步
 @SpringBootApplication
 public class AdminApplication {
 	public static void main(String[] args) {
