25ad1e8d73317e6bd13daf20fdde14b113ec1b4a
张旭升 <2744932552@qq.com>
2026-06-25 15:23:56 +0800

    测试

--- admin/backen/src/main/java/com/neuCloudBrainMedical/admin/AdminApplication.java
+++ admin/backen/src/main/java/com/neuCloudBrainMedical/admin/AdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdminApplication {
-
+//测试0021
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
 	}
